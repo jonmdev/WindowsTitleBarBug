fb6042a8eb5455bb8f2570b60700092adc8fdd9f
mjondev <43948450+jonmdev@users.noreply.github.com>
2024-03-03 23:52:55 -0500

    Initial commit

---
+++ WindowsTitleBarBug/App.xaml.cs
@@ -0,0 +1,55 @@
+﻿
+using System.Diagnostics;
+
+namespace WindowsTitleBarBug {
+    public partial class App : Application {
+        public App() {
+            InitializeComponent();
+
+            //BASIC LAYOUT
+            ContentPage contentPage = new ContentPage();
+            MainPage = contentPage;
+
+            AbsoluteLayout absoluteLayout = new AbsoluteLayout();
+            absoluteLayout.BackgroundColor = Colors.Red;
+            contentPage.Content = absoluteLayout;
+
+            Image image = new Image();
+            Size imgSize = new Size(800, 526);
+            image.Source = "beach.jpg";
+            absoluteLayout.Add(image);
+
+            AbsoluteLayout topBar = new AbsoluteLayout();
+            topBar.BackgroundColor = Colors.White;
+            topBar.HeightRequest = 40;
+            absoluteLayout.Add(topBar); 
+
+            //RESIZER
+            contentPage.SizeChanged += delegate {
+                if (contentPage.Height > 0) {
+                    absoluteLayout.HeightRequest = contentPage.Height;
+                    absoluteLayout.WidthRequest = contentPage.Width;
+
+                    image.WidthRequest = contentPage.Width;
+                    image.HeightRequest = (imgSize.Height / imgSize.Width) * image.WidthRequest;
+
+                    topBar.WidthRequest = contentPage.Width;
+                }
+            };
+
+            //OSCILLATOR
+            var timer = Application.Current.Dispatcher.CreateTimer();
+            timer.Interval = TimeSpan.FromSeconds(1/30.0);
+            DateTime lastDateTime = DateTime.Now;
+            double timerTime = 0;
+            timer.Tick += delegate {
+                image.TranslationY = Math.Sin(Math.Tau * 0.3 * timerTime) * 300;
+                timerTime += (DateTime.Now - lastDateTime).TotalSeconds;
+                lastDateTime = DateTime.Now;
+            };
+            timer.Start();
+            
+
+        }
+    }
+}
---
+++ WindowsTitleBarBug/MauiProgram.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
+using Microsoft.Maui.Platform;
+
+#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+#endif
+
+namespace WindowsTitleBarBug {
+    public static class MauiProgram {
+        public static MauiApp CreateMauiApp() {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts => {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+
+#if WINDOWS
+            //https://stackoverflow.com/questions/71806578/maui-how-to-remove-the-title-bar-and-fix-the-window-size //temporary solution, if make full screen no mismatch pixels
+            builder.ConfigureLifecycleEvents(events =>
+            {
+                events.AddWindows(lifeCycleBuilder =>
+                {
+                    lifeCycleBuilder.OnWindowCreated(w =>
+                    {
+                        w.ExtendsContentIntoTitleBar = false;
+                        IntPtr wHandle = WinRT.Interop.WindowNative.GetWindowHandle(w);
+                        WindowId windowId = Win32Interop.GetWindowIdFromWindow(wHandle);
+                        AppWindow appWindow = AppWindow.GetFromWindowId(windowId);
+                        var titleBar = appWindow.TitleBar;                                
+                        titleBar.BackgroundColor = Microsoft.Maui.Graphics.Colors.GreenYellow.ToWindowsColor();
+                        titleBar.ButtonBackgroundColor = Microsoft.Maui.Graphics.Colors.GreenYellow.ToWindowsColor();                                             
+                        titleBar.InactiveBackgroundColor = Microsoft.Maui.Graphics.Colors.LightGrey.ToWindowsColor();    
+                        titleBar.ButtonInactiveBackgroundColor = Microsoft.Maui.Graphics.Colors.DarkGrey.ToWindowsColor();
+                        //appWindow.SetPresenter(AppWindowPresenterKind.FullScreen);  // TO SET THE APP INTO FULL SCREEN
+                        appWindow.SetPresenter(AppWindowPresenterKind.Default);  // TO NOT SET THE APP INTO FULL SCREEN
+                        appWindow.Resize (new SizeInt32(1500, 700));
+                        appWindow.Move(new PointInt32(0, 0));
+                    });
+                    
+                });
+            });
+
+#endif
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
